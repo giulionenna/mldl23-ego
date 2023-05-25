fdc0cdf86bfcfa5a6add822a3f6f8e9f41a90fd8
giulio524 <62382801+giulionenna@users.noreply.github.com>
2023-05-25 17:53:57 +0200

    solved colab gpu

--- train_classifier_ta3n.py
+++ train_classifier_ta3n.py
@@ -50,10 +50,8 @@ def main():
     #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if(args.gpus == "mps:0"):
         device = torch.device("mps:0" if torch.backends.mps.is_available() else "cpu")
-    elif(args.gpus == "cuda"):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     else: 
-        device = torch.device("cpu")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logger.info('Using device '+str(device))
     # these dictionaries are for more multi-modal training/testing, each key is a modality used
     models = {}
