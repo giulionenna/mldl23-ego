AveragePools fixed

models/FinalClassifier.py
@@ -28,7 +28,7 @@ class Classifier(nn.Module):
         n_gsf_out = 512
         self.n_gsf_out = n_gsf_out
         
-
+        #Method 1
         self.gsf_vec = nn.ModuleList()
         for i in range(self.n_feat[0]):
             gsf = nn.Sequential()
@@ -41,19 +41,33 @@ class Classifier(nn.Module):
             gsf.add_module('gsf_relu2', nn.LeakyReLU(0.1))
            
             self.gsf_vec += [gsf]
+        #Method 2
+        #self.gsf = nn.Sequential()
+        #self.gsf.add_module('gsf_fc1', nn.Linear(self.n_feat[1], n_gsf_out))
+        #self.gsf.add_module('gsf_bn1', nn.BatchNorm1d(n_features[0]))
+        #self.gsf.add_module('gsf_relu1', nn.LeakyReLU(0.1))
+        #self.gsf.add_module('gsf_drop1', nn.Dropout())
+        #self.gsf.add_module('gsf_fc2', nn.Linear(n_gsf_out, n_gsf_out))
+        #self.gsf.add_module('gsf_bn2', nn.BatchNorm1d(n_features[0]))
+        #self.gsf.add_module('gsf_relu2', nn.LeakyReLU(0.1))
 
         #Spatial Domain Discriminator
         if(ablation_mask["gsd"]):
             n_gsd = 256;
-            self.gsd = nn.Sequential()
-            self.gsd.add_module('gsd_fc1', nn.Linear(n_gsf_out*n_features[0], n_gsd))
-            self.gsd.add_module('gsd_bn1', nn.BatchNorm1d(n_gsd))
-            self.gsd.add_module('gsd_relu1', nn.LeakyReLU(0.1))
-            self.gsd.add_module('gsd_drop1', nn.Dropout())
-            self.gsd.add_module('gsd_fc2', nn.Linear(n_gsd, n_gsd//2))
-            self.gsd.add_module('gsd_bn2', nn.BatchNorm1d( n_gsd//2))
+            #self.gsd = nn.Sequential()
+            #self.gsd.add_module('gsd_fc1', nn.Linear(n_gsf_out*n_features[0], n_gsd))
+            #self.gsd.add_module('gsd_bn1', nn.BatchNorm1d(n_gsd))
+            #self.gsd.add_module('gsd_relu1', nn.LeakyReLU(0.1))
+            #self.gsd.add_module('gsd_drop1', nn.Dropout())
+            #self.gsd.add_module('gsd_fc2', nn.Linear(n_gsd, n_gsd//2))
+            #self.gsd.add_module('gsd_bn2', nn.BatchNorm1d( n_gsd//2))
+            #self.gsd.add_module('gsd_relu2', nn.LeakyReLU(0.1))      
+            #self.gsd.add_module('gsd_fc3', nn.Linear( n_gsd//2, 2))
+            #Method 2 Chinese
+            self.gsd.add_module('gsd_fc2', nn.Linear(n_gsf_out*n_features[0], n_gsd))
+            self.gsd.add_module('gsd_bn2', nn.BatchNorm1d( n_gsd))
             self.gsd.add_module('gsd_relu2', nn.LeakyReLU(0.1))      
-            self.gsd.add_module('gsd_fc3', nn.Linear( n_gsd//2, 2))
+            self.gsd.add_module('gsd_fc3', nn.Linear( n_gsd, 2))
             
         
         #Temporal Pooling
@@ -77,10 +91,11 @@ class Classifier(nn.Module):
                         )
                     self.grd_all += [grd]
         
-        self.AvgPool = nn.AdaptiveAvgPool2d((1,n_gsf_out))
+        self.AvgPool = nn.AvgPool2d([self.n_feat[0],1])
         #Temporal Domain discriminator
         if(ablation_mask["gtd"]):
             n_gtd = 512
+            #Method 1
             self.gtd = nn.Sequential()
             self.gtd.add_module('gtd_fc1',     nn.Linear(n_gsf_out, n_gtd))
             self.gtd.add_module('gtd_bn1',     nn.BatchNorm1d(n_gtd))
@@ -90,7 +105,13 @@ class Classifier(nn.Module):
             self.gtd.add_module('gtd_bn2',     nn.BatchNorm1d(n_gtd//2))
             self.gtd.add_module('gtd_relu2',   nn.LeakyReLU(0.1))      
             self.gtd.add_module('gtd_fc3',     nn.Linear(n_gtd//2, 2))
-            
+            #Method 2
+            #self.gtd = nn.Sequential()
+            #self.gtd.add_module('gtd_fc1',     nn.Linear(n_gsf_out, n_gtd))
+            #self.gtd.add_module('gtd_bn1',     nn.BatchNorm1d(n_gtd))
+            #self.gtd.add_module('gtd_relu1',   nn.LeakyReLU(0.1))
+            #self.gtd.add_module('gtd_drop1',   nn.Dropout())
+            #self.gtd.add_module('gtd_fc2',     nn.Linear(n_gtd, 2))  
         
         #Gy
         #1
@@ -109,9 +130,14 @@ class Classifier(nn.Module):
         temporal_domain_out = None
         grd_outs = None
         class_out = None
+        #GSF 
+        # method 1
         gsf_out = torch.zeros(x.shape[0], self.n_feat[0], self.n_gsf_out).to(self.device)
         for i in range(self.n_feat[0]):
             gsf_out[:,i,:] = self.gsf_vec[i](x[:,i,:])
+        
+        #method 2
+        #gsf_out = self.gsf(x)
     
         #spatial domain out
         if(self.ablation_mask["gsd"]):
@@ -136,18 +162,19 @@ class Classifier(nn.Module):
                     weights = 1-entropy
                     weights = weights.unsqueeze(2).repeat(1,1,TRN_out.shape[2]) #[32,4] -> [32,4,#feat] on dim=2 repeate the element of the second dim
 
+                    TRN_weighted = (weights+1)*TRN_out
+                    temporal_aggregation = nn.AvgPool2d([4,1])(TRN_weighted.unsqueeze(1)).squeeze()
+                    #weighted_to_avg = ((weights+1)*TRN_out).transpose(1,2)
+                    #temporal_aggregation = nn.AvgPool1d(4)(weighted_to_avg)
+                    #temporal_aggregation = temporal_aggregation.squeeze(2)
 
-                    #temporal_aggregation = torch.sum((weights+1)*TRN_out,dim=1)
-                    weighted_to_avg = ((weights+1)*TRN_out).transpose(1,2)
-                    temporal_aggregation = nn.AvgPool1d(4)(weighted_to_avg)
-                    temporal_aggregation = temporal_aggregation.squeeze(2)
                 else:
-                    temporal_aggregation = self.AvgPool(TRN_out).reshape(TRN_out.shape[0],TRN_out.shape[2])
+                    temporal_aggregation = nn.AvgPool2d(TRN_out.unsqueeze(1)).squeeze()
             else:
                 #temporal_aggregation = self.AvgPool(TRN_out).reshape(TRN_out.shape[0],TRN_out.shape[2])
                 temporal_aggregation = torch.sum(TRN_out,dim=1)
         else:
-            temporal_aggregation =self.AvgPool(gsf_out).reshape(gsf_out.shape[0],gsf_out.shape[2])
+            temporal_aggregation =self.AvgPool(gsf_out.unsqueeze(1)).squeeze()
         #temporal domain
         if(self.ablation_mask["gtd"]):
             temporal_domain_out =  self.gtd(ReverseLayerF.apply(temporal_aggregation,alpha))

train_classifier_multimodal.py
@@ -160,6 +160,7 @@ def main_train(temporal_type = None, ablation = None, loss_weights = None, shift
         torch.save(loss_train, loss_file_name)
         
         score = {'best': best_score, 'last': last_score}
+        wandb.finish()
         return loss_train, score
         
     elif args.action == "validate":
@@ -301,7 +302,8 @@ def train(action_classifier, train_loader, target_loader,val_loader, device, num
                 action_classifier.best_iter_score = val_metrics['top1']
 
             if args.wandb_name is not None:
-                wandb.log({"acc":  action_classifier.best_iter_score})
+                wandb.log({"best_acc":  action_classifier.best_iter_score})
+                wandb.log({"acc_top1":  val_metrics['top1']})
             action_classifier.save_model(real_iter, val_metrics['top1'], prefix=None)
             action_classifier.train(True)
 